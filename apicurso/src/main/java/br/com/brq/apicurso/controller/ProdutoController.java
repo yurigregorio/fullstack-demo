310a06d59c2fd34f874b7ffdc1d37f053de7b31b
yurigregorio <68123182+yurigregorio@users.noreply.github.com>
2020-09-24 11:59:02 -0300

    Paginacao

--- apicurso/src/main/java/br/com/brq/apicurso/controller/ProdutoController.java
+++ apicurso/src/main/java/br/com/brq/apicurso/controller/ProdutoController.java
@@ -2,13 +2,17 @@ package br.com.brq.apicurso.controller;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.brq.apicurso.model.Produto;
@@ -45,5 +49,14 @@ public class ProdutoController {
 	@DeleteMapping (value = "produtos/{id}")
 	public void delete(@PathVariable int id){
 		this.produtoService.delete(id);
+	}
+	
+	@GetMapping (value = "produtos/paginador")
+	public Page<Produto> paginacao(
+			@RequestParam (value="pagina", defaultValue ="0") int pagina,
+			@RequestParam (value="linhas", defaultValue ="5") int linhas
+			) {
+		return this.produtoService.paginacao(pagina, linhas);
+		
 	}
 }
--- apicurso/src/main/java/br/com/brq/apicurso/service/ProdutoService.java
+++ apicurso/src/main/java/br/com/brq/apicurso/service/ProdutoService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.brq.apicurso.model.Categoria;
@@ -64,6 +66,12 @@ public class ProdutoService {
 	//delete
 	public void delete(int id) {
 		this.produtoRepository.deleteById(id);
+	}
+	
+	public Page<Produto> paginacao(int pagina, int linhas){
+		PageRequest pageRequest = PageRequest.of(pagina, linhas);
+		
+		return this.produtoRepository.findAll(pageRequest);
 	}
 	
 	

97f45cbbbd5c42d2dedb6df3b9e2fd6715d5a904
yurigregorio <yuri.gregorio1@hotmail.com>
2020-09-25 17:46:31 -0300

    Atualizando Aula 1 de Angular

--- frontend/src/app/aluno/aluno.module.ts
+++ frontend/src/app/aluno/aluno.module.ts
@@ -1,4 +1,4 @@
-commimport { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AlunoListComponent } from './aluno-list/aluno-list.component';
 

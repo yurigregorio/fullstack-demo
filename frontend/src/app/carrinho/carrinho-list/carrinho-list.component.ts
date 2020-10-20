d449396d5e8d4feb4a005bf065f9c1b6028d1e77
yurigregorio <yuri.gregorio1@hotmail.com>
2020-10-20 11:08:49 -0300

    Adicionado Subtotal Carrinho

--- frontend/src/app/carrinho/carrinho-list/carrinho-list.component.ts
+++ frontend/src/app/carrinho/carrinho-list/carrinho-list.component.ts
@@ -9,19 +9,36 @@ import { StorageService } from 'src/app/shared/services/storage.service';
 export class CarrinhoListComponent implements OnInit {
 
   public carrinhoApi : any = [];
-  public totalCarrinho : number;
+  public totalCarrinho : number = 0;
 
   constructor(private storageService : StorageService) { }
 
   ngOnInit(): void {
     this.carrinhoApi = this.storageService.getCarrinho();
     console.log(this.carrinhoApi);
-    
+    for (let i = 0; i < this.carrinhoApi.length; i++) {
+        this.totalCarrinho = (this.carrinhoApi[i].produto.preco * this.carrinhoApi[i].quantidade) + this.totalCarrinho;
+        console.log(this.totalCarrinho);
+    }
+
+
   }
 
   deletar(produto){
     let index = this.carrinhoApi.findIndex( x => { return x.produto.id == produto.id });
     this.carrinhoApi.splice(index,1);
+    this.storageService.setCarrinho(this.carrinhoApi);
+  }
+
+  atualizarTotal(){
+    this.totalCarrinho = 0;
+    for (let i = 0; i < this.carrinhoApi.length; i++) {
+      this.totalCarrinho = (this.carrinhoApi[i].produto.preco * this.carrinhoApi[i].quantidade) + this.totalCarrinho;
+      console.log(this.totalCarrinho);
+    }
+  }
+
+  atualizaCarrinho(){
     this.storageService.setCarrinho(this.carrinhoApi);
   }
 
